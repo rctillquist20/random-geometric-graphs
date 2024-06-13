7d988d901f6f26234c4b74f5cb8e8a23024df1be
Evan Alba <evanalba@protonmail.com>
2024-06-12 21:15:00 -0700

    feature: Working but a seed bug in running experiments.

---
+++ experiments.py
@@ -0,0 +1,80 @@
+# Python code to collect data related to the metric dimension of random geometric graphs
+
+import numpy as np
+import networkx as nx
+import multilateration as geo
+from itertools import product
+import multiprocessing as mp
+import pickle
+import glob
+
+##################
+### READ/WRITE ###
+##################
+
+
+def readList(inFile):
+    L = []
+    with open(inFile, 'rb') as f:
+        L = pickle.load(f)
+    return L
+
+
+def writeFile(data, outFile):
+    with open(outFile, 'wb') as o:
+        pickle.dump(data, o, protocol=pickle.HIGHEST_PROTOCOL)
+
+#############
+### UTILS ###
+#############
+# determines the number of additional repeats necessary for each n,r pair in pairs
+# input: data - a list of tuples (n, r, seed, resSet)
+#       pairs - a list of tuples (n, r) to consider
+#       repeats - the target number of repeats for each (n,r) pair
+# return a dictionary (n,r)->(repeats - number of occurences in data)
+
+
+def countComplete(data, pairs, repeats):
+    counts = {pair: repeats for pair in pairs}
+    for (n, r, _, _) in data:
+        if (n, r) in counts:
+            counts[(n, r)] -= 1
+    return counts
+
+
+if __name__ == '__main__':
+    dataFile = 'rgg_data.list'
+    data = readList(dataFile) if glob.glob(dataFile) else []
+
+    # Node Sizes
+    nList = [4]
+
+    # Radius Amounts
+    rList = [1]
+    repeats = 2
+
+    # loop over all pairs of n and r (this can be done with nested loops or the product function in itertools)
+    # look through data to see how many more repeats we need of this n,r pair (repeats-(the number in data))
+    # ..the countComplete function might help here
+    # use np.random.randint(1, 1000000, size=(the number of additional repeats required)) to generate seeds
+    # use these seeds to generate random graphs, nx.random_geometric_graph(n, r, seed=seed)
+    # use the ich function to find a small resolving set
+    # append (n, r, seed, resolving set) to data
+    # save data
+    print('''
+    You know the greatest danger facing us is ourselves, and irrational fear 
+    of the unknown. There is no such thing as the unknown. Only things 
+    temporarily hidden, temporarily not understood.
+    -- Captain Kirk
+    ''')
+    for n in nList:
+        for r in rList:
+            for seed in np.random.randint(1, 1000000, size=repeats):
+                print(type(seed))
+                # TODO: Fix seed=seed error
+                # G = nx.random_geometric_graph(n, r, seed=seed)
+                # resSet = geo.ich(G)
+                # data.append((n, r, seed, resSet))
+
+    print('Experiments Complete!\n')
+    # print(resSet)
