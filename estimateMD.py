091c65c3cf9c4bfc1e0f986392104c385419230e
Matthew Hernandez <mjhernandez6@csuchico.edu>
2024-07-05 10:47:35 -0700

    renamed predictMD.py

--- estimateMD.py
+++ estimateMD.py
@@ -1,19 +1,21 @@
 #Author: Matthew J. Hernandez
-#Updated: 2024-07-03
+#Updated: 2024-07-05
 #This is based off of the data collected by Dr. Richard Carter Tillquist
 
-n = 0
-r = 0.0
+n = 0 #number of nodes
+r = 0.0 #size of radius
+
+x = {"name" : "John", "age" : 36} #Sample dict in Python
 
 #This function estimates and prints the Metric Dimension of a graph given the number of nodes and the desired radius
-def estimatedMD(n, r):
+def estimateMD(n, r):
     if (n >= 0 & n <= 1):
         metricDimension = 0
     print("The estimated Metric Dimension for your graph with " + n + " nodes and a radius of " + r + " is: " )
     
 #2d array or matrix
 #nodes on top
-#radii
+#radii on left
 
 #look at Evan's getDistanceMatrix file in his branch
 #can filter columns
@@ -24,4 +26,6 @@ def estimatedMD(n, r):
 
 n = input("What is the number of nodes for your graph?\n")
 r = input("What is the given radius?\n")
-estimatedMD(n, r)
+print("The estimated Metric Dimension for your graph with " + n + " nodes and a radius of " + r + " is: " + estimateMD(n, r) )
+
+
